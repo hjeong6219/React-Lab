99ad5d2777362dce63b26ed544e89d43797ae3b9
Josh <it.josh.jeong@gmail.com>
2023-09-16 16:53:36 -0400

    converted key down functions into custom hook

---
+++ usepopcorn/src/useLocalStorageState.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export function useLocalStorageState(initialState, key) {
+  const [value, setValue] = useState(function () {
+    return localStorage ? JSON.parse(localStorage.getItem(key)) : initialState;
+  });
+
+  useEffect(
+    function () {
+      localStorage.setItem(key, JSON.stringify(value));
+    },
+    [value, key]
+  );
+
+  return [value, setValue];
+}
