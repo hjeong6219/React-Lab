be69b6985b55f2ce9cea1e4c0a2e6dd4fb835188
hjeong6219 <it.josh.jeong@gmail.com>
2023-09-17 15:50:26 -0400

    added tests

--- codesplain/src/test/server.js
+++ codesplain/src/test/server.js
@@ -11,11 +11,13 @@ export function createServer(handlerConfig) {
 
   beforeAll(() => {
     server.listen();
+    console.log('A server is listening')
   });
   beforeEach(() => {
     server.resetHandlers();
   });
   afterAll(() => {
     server.close();
+    console.log('A server is closed!')
   });
 }
