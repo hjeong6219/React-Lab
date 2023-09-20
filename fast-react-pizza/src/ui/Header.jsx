3c955484b62000b1bff2605f8b2e865f5bcd5ccb
Josh <it.josh.jeong@gmail.com>
2023-09-20 13:00:08 -0400

    fixed linter and implemented router

---
+++ fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>Josh</p>
+    </div>
+  );
+}
+
+export default Header;
