6fb186b3cedec2632fba90fad2c80bd224c514a3
Josh <it.josh.jeong@gmail.com>
2023-09-20 18:06:25 -0400

    styling buttons

--- fast-react-pizza/src/ui/Button.jsx
+++ fast-react-pizza/src/ui/Button.jsx
@@ -1,16 +1,21 @@
 import { Link } from 'react-router-dom';
 
-function Button({ children, disabled, to }) {
-  const className =
-    'inline-block px-4 py-3 font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 md:py-4';
+function Button({ children, disabled, to, type }) {
+  const base =
+    'inline-block  font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ';
+
+  const styles = {
+    primary: base + 'px-4 py-3 md:px-6 md:py-6',
+    small: base + 'px-4 py-2 md:px-5 md:py-2.5 text-xs',
+  };
   if (to)
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
   return (
-    <button className={className} disabled={disabled}>
+    <button className={styles[type]} disabled={disabled}>
       {children}
     </button>
   );
