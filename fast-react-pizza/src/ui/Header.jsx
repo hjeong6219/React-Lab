d17f3b52bd9cb0219d06def0e2d6548121ec75c0
Josh <it.josh.jeong@gmail.com>
2023-09-20 20:33:42 -0400

    adding username dynamically

--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
 function Header() {
   return (
@@ -8,6 +9,7 @@ function Header() {
         Fast React Pizza Co.
       </Link>
       <SearchOrder className="" />
+      <Username />
     </header>
   );
 }
