05942b9e0341335ccba351eaaf4b18e6e3835226
Josh <it.josh.jeong@gmail.com>
2023-09-22 23:51:57 -0400

    implemented function to fetch cabin data from db

--- the-wild-oasis/src/pages/Cabins.jsx
+++ the-wild-oasis/src/pages/Cabins.jsx
@@ -2,17 +2,20 @@ import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img src="https://lybrkcjuccvuxhejoksl.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2023-09-23T03%3A08%3A43.411Z" />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter/Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
