a5990e24e94743141f58e0d8b633d8570c03902d
Josh <it.josh.jeong@gmail.com>
2023-09-22 22:38:12 -0400

    implemented supabase

---
+++ the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://lybrkcjuccvuxhejoksl.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5YnJrY2p1Y2N2dXhoZWpva3NsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTUzODY3OTYsImV4cCI6MjAxMDk2Mjc5Nn0.t-mMm8ScgPgTzcoot8vx9332ifFQLWRMMOPjIVC5z0s";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
